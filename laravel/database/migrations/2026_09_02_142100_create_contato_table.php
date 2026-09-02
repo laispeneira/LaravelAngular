4470155fa4f250b49eae8c86c6309beac7640443
laispeneira <llaispereiraa2303@gmail.com>
2026-09-02 11:37:21 -0300

    commit 1 mifrate

--- laravel/database/migrations/2026_09_02_142100_create_contato_table.php
+++ laravel/database/migrations/2026_09_02_142100_create_contato_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('contato', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nome', 255);
+            $table->string('email', 255);
         });
     }
 
