11d10e8f97e587e0e17691d78708961cca041d67
laispeneira <llaispereiraa2303@gmail.com>
2026-09-02 12:22:25 -0300

    commit

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,14 +14,14 @@ use App\Http\Middleware\LogAcessoMiddleware;
 */
 
 Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
-Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato'])->middleware(LogAcessoMiddleware::class);
 
 
 Route::prefix('/aluno')->group(function(){
     Route::get('/index', [App\Http\Controllers\AlunoController::class, 'index'])->name('aluno.index');
     Route::get('/create', [App\Http\Controllers\AlunoController::class, 'create'])->name('aluno.create');
     Route::post('/show', [App\Http\Controllers\AlunoController::class, 'show'])->name('aluno.show');
-	  Route::post('/edit', [App\Http\Controllers\AlunoController::class, 'edit'])->name('aluno.edit');
-	  Route::post('/update', [App\Http\Controllers\AlunoController::class, 'update'])->name('aluno.update');
+	Route::post('/edit', [App\Http\Controllers\AlunoController::class, 'edit'])->name('aluno.edit');
+	Route::post('/update', [App\Http\Controllers\AlunoController::class, 'update'])->name('aluno.update');
     Route::post('/destroy', [App\Http\Controllers\AlunoController::class, 'destroy'])->name('aluno.destroy');
 });
